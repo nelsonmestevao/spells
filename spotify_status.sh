91ebc45ec3f70353c7bd3d490642ab461f241532
Nelson Estevão <nelsonmestevao@gmail.com>
2018-08-09 20:17:31 +0100

    Remove icon from script output

--- spotify_status.sh
+++ spotify_status.sh
@@ -3,7 +3,7 @@
 if [ "$(playerctl status)" = "Playing" ]; then
     title=$(exec playerctl metadata xesam:title)
     artist=$(exec playerctl metadata xesam:artist)
-  echo " $title - $artist"
+  echo "$title - $artist"
 else
   echo ""
 fi
