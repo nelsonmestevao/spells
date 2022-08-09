12cb4d42565456c7cbf765d6956c7fe7e75d2ab3
Nelson Estevão <nelsonmestevao@proton.me>
2022-08-09 23:21:32 +0100

    Fix natural scroll script

--- i3wm/naturalScrollOn.sh
+++ i3wm/naturalScrollOn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-device=$(xinput list | grep Touchpad | grep -Eo '[0-9]{1,9}' | sed -n 2p)
+device=$(xinput list | grep Touchpad | grep -Eo 'id=([0-9]*)' | awk -F'=' '{ print $2 }')
 
 prop=$(xinput list-props "$device" | grep "Natural Scrolling Enabled" | grep -oP '\(\K[^\])]+' | sed -n 1p)
 
