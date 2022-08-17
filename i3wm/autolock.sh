4b8e0cf51e04bf0ef8c58caf94dda0c51406faf0
Nelson Estevão <nelsonmestevao@proton.me>
2022-08-17 22:54:07 +0100

    [i3wm]: Add autolock script

---
+++ i3wm/autolock.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Only exported variables can be used within the timer's command.
+export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
+
+# Run xidlehook
+xidlehook \
+  `# Don't lock when there's a fullscreen application` \
+  --not-when-fullscreen \
+  `# Dim the screen after 5 minutes, undim if user becomes active` \
+  --timer 300 \
+    'redshift -P -b 0.5' \
+    'redshift -P -b 1' \
+  `# Undim & lock after 5 more minutes` \
+  --timer 300 \
+    '~/.spells/i3wm/manager.sh lock' \
+    '' \
+  `# Finally, suspend an hour after it locks` \
+  --timer 3600 \
+    '~/.spells/i3wm/manager.sh suspend' \
+    ''
