cec4538139049e722723de6c4758770ce096746c
Nelson Estevão <nelsonmestevao@proton.me>
2023-03-28 18:03:40 +0100

    Update i3wm manager

--- i3wm/manager.sh
+++ i3wm/manager.sh
@@ -1,34 +1,32 @@
 #!/usr/bin/env sh
 
-[ "$(cat /proc/1/comm)" = "systemd" ] && logind=systemctl || logind=loginctl
-
 case "$1" in
-    lock)
-        dm-tool lock
-        ;;
-    logout)
-        i3-msg exit
-        ;;
-    switch_user)
-        dm-tool switch-to-greeter
-        ;;
-    suspend)
-        dm-tool lock && $logind suspend
-        ;;
-    hibernate)
-        dm-tool lock && $logind hibernate
-        ;;
-    reboot)
-        $logind reboot
-        ;;
-    shutdown)
-        $logind poweroff
-        ;;
-    *)
-        echo "== ! manager: missing or invalid argument ! =="
-        echo "Try again with: lock | logout | switch_user | suspend | hibernate | reboot | shutdown"
-        exit 2
+  lock)
+    dm-tool lock
+    ;;
+  logout)
+    i3-msg exit
+    ;;
+  switch_user)
+    dm-tool switch-to-greeter
+    ;;
+  suspend)
+    dm-tool lock && systemctl suspend
+    ;;
+  hibernate)
+    dm-tool lock && systemctl hibernate
+    ;;
+  reboot)
+    systemctl reboot
+    ;;
+  shutdown)
+    systemctl poweroff
+    ;;
+  *)
+    echo "== ! manager: missing or invalid argument ! =="
+    echo "Try again with: lock | logout | switch_user | suspend | hibernate | reboot | shutdown"
+    exit 2
+    ;;
 esac
 
 exit 0
-
