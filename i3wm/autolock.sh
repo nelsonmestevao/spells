f0cce9bd2ee67499596480f2efed40cedd7db3af
Nelson Estevão <nelsonmestevao@proton.me>
2023-04-24 14:22:46 +0100

    Update autolock

--- i3wm/autolock.sh
+++ i3wm/autolock.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
 # Only exported variables can be used within the timer's command.
-export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
+# export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
 
 # Run xidlehook
 xidlehook \
   `# Don't lock when there's a fullscreen application` \
   --not-when-fullscreen \
-  `# Undim & lock after 10 minutes` \
+  `# Exit after the whole chain of timer commands have been invoked once` \
+  --once \
+  `# Lock after 10 minutes` \
   --timer 600 \
-    "~/.spells/i3wm/manager.sh lock" \
-    '' \
-  `# Finally, suspend an hour after it locks` \
-  --timer 3600 \
-    "~/.spells/i3wm/manager.sh suspend" \
-    ''
+  "$HOME/.spells/i3wm/manager.sh lock" \
+  ''
