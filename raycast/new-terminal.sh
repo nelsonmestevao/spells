2a030e91e24bcb408bf1a9ad53f7f6595219a2d7
Nelson Estevão <nelson.estevao@remote.com>
2024-07-10 21:52:22 +0100

    chore: add new terminal script for raycast

---
+++ raycast/new-terminal.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title New Terminal
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🖥️
+
+# Documentation:
+# @raycast.description Opens a new Alacritty Window
+
+echo "Opening Alacritty..."
+
+open -n "/Applications/Alacritty.app/"
+
