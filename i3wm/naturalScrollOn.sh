902c9519ab271fd285689c85a8ebdd307c7786f9
Nelson Estevão <nelsonmestevao@proton.me>
2025-08-17 14:06:10 +0100

    chore: remove i3 related scripts

--- i3wm/naturalScrollOn.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-device=$(xinput list | grep Touchpad | grep -Eo 'id=([0-9]*)' | awk -F'=' '{ print $2 }')
-
-prop=$(xinput list-props "$device" | grep "Natural Scrolling Enabled" | grep -oP '\(\K[^\])]+' | sed -n 1p)
-
-xinput --set-prop "$device" "$prop" 1
--- i3wm/spotify_status.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-PLAYER=$(playerctl --list-all)
-
-if [ "$PLAYER" = "spotify" ]; then
-  TITLE=$(exec playerctl metadata xesam:title)
-  ARTIST=$(exec playerctl metadata xesam:artist)
-  echo "$TITLE - $ARTIST"
-fi
